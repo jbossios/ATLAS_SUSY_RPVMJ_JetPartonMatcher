6c23a4579a284c15d35b8ea3b56f0986ee5cf39b
Jonathan Bossio <jbossios@cern.ch>
2022-04-13 13:25:54 +0200

    Push pytest

---
+++ tests/test_all.py
@@ -0,0 +1,15 @@
+from tester import run_tester
+
+def test_RecomputeDeltaRvalues_drPriority():
+  run_tester('RecomputeDeltaRvalues_drPriority')
+
+def test_RecomputeDeltaRvalues_ptPriority():
+  run_tester('RecomputeDeltaRvalues_ptPriority')
+
+def test_UseFTDeltaRvalues():
+  run_tester('UseFTDeltaRvalues')
+
+if __name__ == '__main__':
+  test_RecomputeDeltaRvalues_drPriority()
+  test_RecomputeDeltaRvalues_ptPriority()
+  test_UseFTDeltaRvalues()
